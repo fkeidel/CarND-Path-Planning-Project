5f2f360ae2700774b574dd07d00e177a6b12101b
Frank Keidel <f.keidel@gmx.de>
2018-02-06 10:45:17 +0100

    submit for old websocket version

--- src/main.cpp
+++ src/main.cpp
@@ -243,7 +243,7 @@ int main() {
   double ref_vel = 0;   // start from stop
   int frameNr = 0;
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &ref_vel, &lane, &frameNr](uWS::WebSocket<uWS::SERVER>* ws, char *data, size_t length,
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &ref_vel, &lane, &frameNr](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -455,13 +455,13 @@ int main() {
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
           	//this_thread::sleep_for(chrono::milliseconds(1000));
-          	ws->send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
           
         }
       } else {
         // Manual driving
         std::string msg = "42[\"manual\",{}]";
-        ws->send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
     }
   });
@@ -480,19 +480,18 @@ int main() {
     }
   });
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER>* ws, uWS::HttpRequest req) {
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER>* ws, int code,
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
                          char *message, size_t length) {
-    ws->close();
+    ws.close();
     std::cout << "Disconnected" << std::endl;
   });
 
   int port = 4567;
-  auto host = "127.0.0.1";
-  if (h.listen(host, port))
+  if (h.listen(port))
   {
     std::cout << "Listening to port " << port << std::endl;
   } else {
